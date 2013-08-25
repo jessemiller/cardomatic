c4121a11187e2a6334d0c642fa791557ffc17b4a
Jesse Miller <millerjesse@gmail.com>
2013-08-25 15:08:19 -0700

    Got the cards flipping again

--- Cardomatic/CardomaticViewController.m
+++ Cardomatic/CardomaticViewController.m
@@ -10,20 +10,14 @@
 
 @interface CardomaticViewController ()
 
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cards;
+
 @end
 
 @implementation CardomaticViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)flipCard:(UIButton *)sender {
+    sender.selected = !sender.isSelected;
 }
 
 @end
