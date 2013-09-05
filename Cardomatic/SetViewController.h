bd12dd64549a7767488f0c6192951eea47eb071e
Jesse Miller <millerjesse@gmail.com>
2013-09-04 17:30:42 -0700

    Add new view controller

---
+++ Cardomatic/SetViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetViewController.h
+//  Cardomatic
+//
+//  Created by Jesse Miller on 9/4/13.
+//  Copyright (c) 2013 Jesse Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetViewController : UIViewController
+
+@end
---
+++ Cardomatic/SetViewController.m
@@ -0,0 +1,38 @@
+//
+//  SetViewController.m
+//  Cardomatic
+//
+//  Created by Jesse Miller on 9/4/13.
+//  Copyright (c) 2013 Jesse Miller. All rights reserved.
+//
+
+#import "SetViewController.h"
+
+@interface SetViewController ()
+
+@end
+
+@implementation SetViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
